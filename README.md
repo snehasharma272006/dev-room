abe01ae4192c1b6a3dfbf22ed4744e2eae6e709e
Sneha Sharma <snehasharma272006@gmail.com>
2026-05-23 20:08:04 +0530

    lecture 1 python

---
+++ README.md
@@ -0,0 +1,2 @@
+#dev room
+This is Sneha , documenting her journey from scratch. I'm not a coding prodigyb , but I'm hoping to become one
---
+++ python/lec1.py
@@ -0,0 +1,10 @@
+#determining the type of a variable 
+st = "sneha"
+print (type(st))
+
+#determining the ascii value of a char
+print (ord("a"))
+
+#slicing a string 
+a="python"
+print(a[0:3])
