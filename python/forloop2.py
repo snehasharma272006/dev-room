8a9b41736d2acedf870b32b2644ed42bb40ae718
Sneha Sharma <snehasharma272006@gmail.com>
2026-05-25 21:37:20 +0530

    perfect number code

---
+++ python/forloop2.py
@@ -0,0 +1,13 @@
+# to check wheather a number is a perfet number or not 
+
+factor=0
+n=int(input("enter n "))
+for i in range (1, n):
+    if n%i==0:
+        factor=factor+i
+print("sum of factors of", n, "are", factor)    
+if factor==n:
+     print("no is perfect number")
+else:
+    print("no is not perfect number")
+
